d530c893ff5bc97aad334a2448135fa962f0e78b
VONGUYENQUOCDAT <vodatt165@gmail.com>
2025-02-10 13:48:39 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "library.h"
+
+int main(){
+	int choice;
+	do{
+		displayMenu();
+		scanf("%d",&choice);
+		
+		switch(choice){
+			case 1:
+				displayBooks();
+				break;
+			case 2:
+				addBook();
+				break;
+			case 3:
+				printf("\nThoat chuong trinh.\n");
+				break;
+			default:
+				printf("\nLua chon khong hop le, vui long chon lai.\n");
+		}
+	}while(choice!=3);
+	
+	return 0;
+}
---
+++ menu.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "library.h"
+
+void displayMenu(){
+	printf("\n===== HE THONG QUAN LY THU VIEN =====\n");
+	printf("1. Hien thi danh sach sach\n");
+	printf("2. Them sach\n");
+	printf("3. Thoat\n");
+	printf("Nhap lua chon: ");
+}
